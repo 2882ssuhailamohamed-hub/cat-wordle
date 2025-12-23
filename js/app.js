73f712757c90d12e2635b40effc48d70315e4b49
2882ssuhailamohamed-hub <2882ssuhailamohamed@gmail.com.com>
2025-12-23 11:28:46 +0300

    Cat scene messages for each stage

--- js/app.js
+++ js/app.js
@@ -14,3 +14,14 @@ const WORD_BANK = [
     'CATTY', 'MIAOW', 'PUSSY', 'PAWSY', 'MEOWY',
     'CATSY', 'SLEPY', 'SLEPT', 'PURRY', 'FURRS'
 ];
+
+
+// Cat scene messages for each stage
+const CAT_MESSAGES = [
+    "The cat bed is empty... guess letters to attract a cat! 🛏️",
+    "A cute cat appeared! It's sleeping peacefully. 😴",
+    "The cat woke up and found a yarn ball to play with! 🧶",
+    "Now the cat has a bowl of milk! It's getting happier. 🥛",
+    "Look! The cat is wearing a pretty pink bow! 🎀",
+    "Purr-fect! The cat is so happy and giving you hearts! 💖"
+];
