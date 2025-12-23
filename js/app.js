7be9f997600cb3bf7f27e0660491ea66b2cef1b6
2882ssuhailamohamed-hub <2882ssuhailamohamed@gmail.com.com>
2025-12-23 11:23:44 +0300

    Word Bank

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ const CONFIG = {
     MAX_ATTEMPTS: 6,
     CAT_STAGES: 6
 };
+
+
+
+// Word Bank - ONLY 5-letter cat-related words
+const WORD_BANK = [
+    'KITTY', 'MEOWS', 'PURRS', 'FELINE', 'WHISK',
+    'PAWED', 'TABBY', 'FURRY', 'CLAWS', 'SLEEP',
+    'CATTY', 'MIAOW', 'PUSSY', 'PAWSY', 'MEOWY',
+    'CATSY', 'SLEPY', 'SLEPT', 'PURRY', 'FURRS'
+];
